8a9c7d2aaa454fb706823e9804f55e15264e350f
rachelwoodside <rachel.woodside3@gmail.com>
2024-01-15 13:31:50 -0400

    Enforce log date formatting and use replace function to overwrite log data

--- excel_sheet_helpers.R
+++ excel_sheet_helpers.R
@@ -3,7 +3,8 @@ library(tidyverse)
 library(readxl)
 library(data.table)
 library(glue)
-library(sensorstrings)
+library(writexl)
+library(lubridate)
 
 # File extension extraction function pulled from helpers-misc.R in sensorstrings package on 2024-01-15
 extract_file_extension <- function(file_name) {
@@ -14,7 +15,8 @@ extract_file_extension <- function(file_name) {
   extension$EXT
 }
 
-read_log_data <- function(deployment_folder_path) {
+# TODO: Expand this to work with the config table as well?
+extract_log_file_name <- function(deployment_folder_path) {
   # Read in log as in ss_read_log.R from sensorstrings package on 2024-01-15
   # extract the name of the log folder (e.g. Log, log, LOG)
   log_folder <- list.files(deployment_folder_path) %>%
@@ -36,25 +38,80 @@ read_log_data <- function(deployment_folder_path) {
   else if (length(dat_files) == 0) {
     stop("No files found in the Log folder")
   }
-  
-  # file extension
-  file_type <- extract_file_extension(dat_files)
-  
+  return(glue("{log_path}/{dat_files}"))
+}
+
+read_log_data <- function(log_path, file_type) {
   if (file_type == "xls" | file_type == "xlsx") {
-    log <- read_excel(paste(log_path, dat_files, sep = "/"), na = c("", "n/a", "N/A"))
+    log <- read_excel(log_path, na = c("", "n/a", "N/A"))
   }
   else if (file_type == "csv") {
     log <- fread(
-      paste(log_path, dat_files, sep = "/"),
+      log_path,
       data.table = FALSE,
       na.strings = c("", "n/a", "N/A")
-      )
+    )
   }
   else
   {
-    stop("No .xls or .xlsx files found in the Log folder")
+    stop("No .xls, .xlsx, or .csv files found in the Log folder")
   }
+  return(log)
 }
 
-# testing read_log_data
-read_log_data("R:/program_documents/cmp_hiring/intern/2023_rachel/projects/cmp/deployment_change_tracking/deployment_change_code/updatess/fake_station_folders/birchy_head/birchy_head_2018-02-20")
+write_log_data <- function(updated_log_data, log_path, file_type) {
+  #writexl currently only supports writing the whole file
+  if (file_type == "xls" | file_type == "xlsx") {
+    log <- write_xlsx(updated_log_data, log_path)
+  }
+  #fwrite is also currently easiest to use by overwriting the whole file
+  else if (file_type == "csv") {
+    log <- fwrite(
+      updated_log_data,
+      log_path
+    )
+  }
+  else
+  {
+    stop("No .xls, .xlsx, or .csv files found in the Log folder")
+  }
+  return(log)
+  
+}
+
+update_log_data <- function(deployment_folder_path, column_to_update, old_value, new_value) {
+  # retrieve log file name
+  log_file_name <- extract_log_file_name(deployment_folder_path)
+  # get file type
+  log_file_extension <- extract_file_extension(log_file_name)
+  # read in log data
+  log_data <- read_log_data(log_file_name, log_file_extension)
+  date_formatted_log_data <- log_data %>% 
+                              mutate(Deployment = ymd(Deployment)) %>%
+                              mutate(Retrieval = ymd(Retrieval))
+  # TODO: Check that the log value is in fact the provided old value to make sure we are looking at the right column?
+  updated_log_data <- date_formatted_log_data %>% 
+                      mutate_at(vars(column_to_update), ~ replace(., TRUE, new_value))
+  # write new data to log file 
+  write_log_data(updated_log_data, log_file_name, log_file_extension)
+  return(log_data)
+}
+
+# Error occurring with mutate when working with .xlsx log file and trying to write dates (Deployment column)
+# Can write changes to text successfully, but overwrites dates with weird format
+deployment_folder_path <- "R:/program_documents/cmp_hiring/intern/2023_rachel/projects/cmp/deployment_change_tracking/deployment_change_code/updatess/fake_station_folders/birchy_head/birchy_head_2022-05-12"
+log_file_name <- extract_log_file_name(deployment_folder_path)
+log_file_extension <- extract_file_extension(log_file_name)
+log_data <- read_log_data(log_file_name, log_file_extension)
+date_formatted_log_data <- log_data %>% 
+                            mutate(Deployment = ymd(Deployment)) %>%
+                            mutate(Retrieval = ymd(Retrieval))
+updated_log_data <- date_formatted_log_data %>%
+                    mutate_at(vars("Deployment"), ~ replace(., TRUE, "2022-05-12"))
+write_log_data(updated_log_data, log_file_name, log_file_extension)
+
+# testing update_log_data
+updated_log_data <- update_log_data("R:/program_documents/cmp_hiring/intern/2023_rachel/projects/cmp/deployment_change_tracking/deployment_change_code/updatess/fake_station_folders/birchy_head/birchy_head_2022-05-12", 
+                "Location_Description",
+                "Birchy Head",
+                "Birchy Head 1")
