3d48bf89cc5804394e34980f3a98ee4e889dfadf
Rachel Woodside <rachel.woodside3@gmail.com>
2024-02-08 12:02:01 -0400

    Update excel and csv reading to include NA string as null

--- excel_sheet_helpers.R
+++ excel_sheet_helpers.R
@@ -10,13 +10,13 @@ source("file_system_helpers.R")
 read_spreadsheet_data <- function(file_path, file_type) {
   message(glue("Reading spreadsheet data from: {file_path}"))
   if (file_type == "xls" | file_type == "xlsx") {
-    file <- read_excel(file_path, na = c("", "n/a", "N/A"))
+    file <- read_excel(file_path, na = c("", "n/a", "N/A", "NA"))
   }
   else if (file_type == "csv") {
     file <- fread(
       file_path,
       data.table = FALSE,
-      na.strings = c("", "n/a", "N/A")
+      na.strings = c("", "n/a", "N/A", "NA")
     )
   }
   else
